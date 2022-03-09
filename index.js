ab2a1952b11470235e41a5dd0a8d1acf23096d1c
Aidana777 <aidananiazalieva27@gmail.com>
2022-03-09 13:12:28 +0600

    cool

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const resetBtn=document.getElementById("resetBtn");
     
 let intervalId;
 
-let count=10;
-
 startBtn.onclick=()=>{
     intervalId=setInterval(()=>{
      resultTotal.textContent=numberInput.value
-    },timeInput.value)
+    },++timeInput.value)
 }
 resetBtn.onclick=()=>{
     clearInterval(intervalId)
+    }
+
+
+ let fromDate=document.getElementById("fromDate");
+let untilDate=document.getElementById("untilDate");
+const calculateBtn=document.getElementById("calculateBtn");
+const totalNum=document.getElementById("total");
+
+calculateBtn.onclick=()=>{
+    const one=new Date(fromDate)
+    const two=new Date(untilDate)
+    const time=Math.abs(one-two);
+    const days=Math.ceil(time/(1000*60*60*24));
+    totalNum.innerHTML=days
+
 }
